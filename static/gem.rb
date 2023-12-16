a2d6f1bd5605bb398140e51617dafe415c6ec5b1
gbaptista <guilhermebaptistasilva@gmail.com>
2023-12-16 09:48:39 -0300

    bump to 2.1.0

--- static/gem.rb
+++ static/gem.rb
@@ -3,10 +3,10 @@
 module Gemini
   GEM = {
     name: 'gemini-ai',
-    version: '2.0.0',
+    version: '2.1.0',
     author: 'gbaptista',
     summary: "Interact with Google's Gemini AI.",
-    description: "A Ruby Gem for interacting with Gemini through Vertex AI or AI Studio, Google's generative AI services.",
+    description: "A Ruby Gem for interacting with Gemini through Vertex AI, Generative Language API, or AI Studio, Google's generative AI services.",
     github: 'https://github.com/gbaptista/gemini-ai',
     gem_server: 'https://rubygems.org',
     license: 'MIT',
